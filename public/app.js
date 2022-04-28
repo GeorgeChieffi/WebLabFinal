e5fa861e253fc8cb0a75ca1307e09810b70216eb
Richter Loaner <richter_loaner@RL-8N4Z9C3.cgcent.miami.edu>
2022-04-27 23:42:04 -0400

    added the js for the grid

---
+++ public/app.js
@@ -0,0 +1,17 @@
+const gridTemplate = document.getElementById("grid");
+
+var data = [
+    ["<img src=\"./Puppy2.jpg\"></img>", "<img src=\"./Puppy2.jpg\"></img>", "<img src=\"./Puppy2.jpg\"></img>"],
+    ["<img src=\"./Puppy2.jpg\"></img>", "<img src=\"./Puppy2.jpg\"></img>", "<img src=\"./Puppy2.jpg\"></img>"],
+    ["<img src=\"./Puppy2.jpg\"></img>", "<img src=\"./Puppy2.jpg\"></img>", "<img src=\"./Puppy2.jpg\"></img>"]
+];
+
+console.log("before loop");
+for(let i of data) {
+    for(let j of i) {
+        let cell = document.createElement("div");
+        cell.innerHTML = j;
+        cell.className = "cell";
+        gridTemplate.appendChild(cell);
+    }
+}

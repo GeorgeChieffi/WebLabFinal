1763eabce369e3dc486bb8f0310af8688b8fd98c
Richter Loaner <richter_loaner@RL-8N4Z9C3.cgcent.miami.edu>
2022-05-03 18:02:11 -0400

    less hard code in the app.js file & added some styling touches to the main page

--- public/app.js
+++ public/app.js
@@ -1,21 +1,28 @@
 const gridTemplate = document.getElementById("grid");
 
-var data = [
-    ["<a href=\"./Puppy2.html\"><img src=\"./Images/Puppy2.jpg\"></img></a>", "<a href=\"./Puppy2.html\"><img src=\"./Images/Puppy2.jpg\"></img></a>", "<a href=\"./Puppy2.html\"><img src=\"./Images/Puppy2.jpg\"></img></a>"],
-    ["<a href=\"./Puppy2.html\"><img src=\"./Images/Puppy2.jpg\"></img></a>", "<a href=\"./Puppy2.html\"><img src=\"./Images/Puppy2.jpg\"></img></a>", "<a href=\"./Puppy2.html\"><img src=\"./Images/Puppy2.jpg\"></img></a>"],
-    ["<a href=\"./Puppy2.html\"><img src=\"./Images/Puppy2.jpg\"></img></a>", "<a href=\"./Puppy2.html\"><img src=\"./Images/Puppy2.jpg\"></img></a>", "<a href=\"./Puppy2.html\"><img src=\"./Images/Puppy2.jpg\"></img></a>"]
-];
+for(let i = 0; i < 9; i++) {
+    let cell = document.createElement("div");
+    let aTag = document.createElement("a");
+    aTag.setAttribute("href", "./Puppy2.html");
+    let imgTag = document.createElement("img");
+    imgTag.setAttribute("src", "./Images/Puppy2.jpg")
+    aTag.appendChild(imgTag);
+    cell.appendChild(aTag);
 
-for(let i of data) {
-    for(let j of i) {
-        let cell = document.createElement("div");
-        cell.innerHTML = j;
-        cell.className = "cell";
-        gridTemplate.appendChild(cell);
-    }
+    let puppyDesc = document.createElement("div");
+    let puppyHeader = document.createElement("h1");
+    let puppyText = document.createElement("p");
+    puppyHeader.innerHTML= "Name";
+    puppyText.innerText = "Male, 9\nMiami"
+    puppyDesc.appendChild(puppyHeader);
+    puppyDesc.appendChild(puppyText);
+    cell.appendChild(puppyDesc);
+
+    cell.className = "cell";
+    gridTemplate.appendChild(cell);
 }
 
 let test = document.getElementById("grid");
 test.addEventListener("mouseover" , () => {
-    test.innerHTML("Name");
+    
 });
